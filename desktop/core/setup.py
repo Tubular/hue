edcb25e4a4a26bc104d8daa7f22f81af74886597
pavlo <pavlo@tubularlabs.com>
2017-07-05 13:17:32 +0300

    FIX crontab

--- desktop/core/setup.py
+++ desktop/core/setup.py
@@ -23,7 +23,7 @@ setup(
       description = "Hue core",
       packages = find_packages('src'),
       package_dir = {'': 'src'},
-      install_requires = ['setuptools', 'crontab'],
+      install_requires = ['setuptools'],
 
       entry_points = { 'console_scripts': [ 'supervisor = desktop.supervisor:main',
                                             'hue = desktop.manage_entry:entry', ],
