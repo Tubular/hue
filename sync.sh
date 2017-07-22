e07944ecdb7db826e58e6094ab96c7069785ae7f
pavlo <pavlo@tubularlabs.com>
2017-07-22 07:16:08 +0300

    IMP restore s3 browser

--- sync.sh
+++ sync.sh
@@ -8,3 +8,4 @@ cp -rf /code/apps/metastore/ /opt/hue/apps/
 cp -rf /code/apps/beeswax/ /opt/hue/apps/
 cp -rf /code/desktop/core/src/desktop /opt/hue/desktop/core/src/
 cp -rf /code/desktop/libs/notebook /opt/hue/desktop/libs/
+cp -rf /code/apps/filebrowser/ /opt/hue/apps/
